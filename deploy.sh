19e84b32e142655e10e943c51fee823488609794
Moses Frost <moses@moses.io>
2020-04-14 12:11:54 -0400

    SSHH-copy-id automation

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,8 @@ sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt install ansible sshpass -y
 ssh-keygen
+echo 'Copying Key to infra'
+ssh-copy-id dcloud@198.18.133.5
+echo 'Copying Key to Kali'
+ssh-copy-id root@198.18.133.6
+ansible-playbook -i inventory cdc.yml
