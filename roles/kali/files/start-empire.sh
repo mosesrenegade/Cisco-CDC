71cce4d264342a357a20dbedf44f0c4452d91d2f
Moses Frost <moses@moses.io>
2020-04-28 12:14:57 -0400

    New Start-empire script

---
+++ roles/kali/files/start-empire.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+screen -dmS empire powershell-empire
+echo "--------------                                       "
+echo "Instructions:                                        "
+echo "Use  the screen command to get to your empire shell: "
+echo "root@kali:~# screen -r empire                        "
+echo "Use screen -ls to list any screen sessions           "
+echo "CTRL-a + d will detach it if you need to go back     "
+echo "--------------                                       "
